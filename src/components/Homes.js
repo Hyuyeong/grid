d0c149301c164996598604feee2085009fa78143
Hyuyeong <choibeny@gmail.com>
2022-06-04 21:30:49 +1200

    homes code refactoring

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -11,159 +11,94 @@ import img5 from '../img/house-5.jpeg';
 import img6 from '../img/house-6.jpeg';
 
 const Homes = ({}) => {
-  return (
-    <section className="homes">
-      <div className="home">
-        <img src={img1} alt="House 1" className="home__img" />
+  const data = [
+    {
+      id: 'house 1',
+      name: 'Beautiful Familiy House',
+      location: 'USA',
+      rooms: 5,
+      area: 325,
+      price: 12000000,
+      img: img1,
+    },
+    {
+      id: 'house 2',
+      name: 'Modern Glass Villa',
+      location: 'Canada',
+      rooms: 6,
+      area: 450,
+      price: 2750000,
+      img: img2,
+    },
+
+    {
+      id: 'house 3',
+      name: 'Cozy Country House',
+      location: 'UK',
+      rooms: 4,
+      area: 250,
+      price: 850000,
+      img: img3,
+    },
+    {
+      id: 'house 4',
+      name: 'Large Rustical Villa',
+      location: 'Portugal',
+      rooms: 6,
+      area: 480,
+      price: 1950000,
+      img: img4,
+    },
+    {
+      id: 'house 5',
+      name: 'Majestic Palace House',
+      location: 'Germany',
+      rooms: 18,
+      area: 4230,
+      price: 9500000,
+      img: img5,
+    },
+    {
+      id: 'house 6',
+      name: 'Modern Familiy Apartment',
+      location: 'Italy',
+      rooms: 3,
+      area: 180,
+      price: 600000,
+      img: img6,
+    },
+  ];
+
+  const home = data.map(home => {
+    return (
+      <div className="home" key={home.id}>
+        <img src={home.img} alt={home.id} className="home__img" />
 
         <HeartFull />
-        <h5 className="home__name">Beautiful Familiy House</h5>
+        <h5 className="home__name">{home.name}</h5>
         <div className="home__location">
           <Pin />
-          <p>USA</p>
+          <p>{home.location}</p>
         </div>
         <div className="home__rooms">
           <Male />
-          <p>5 rooms</p>
+          <p>{home.rooms} rooms</p>
         </div>
         <div className="home__area">
           <Expand />
           <p>
-            325 m<sup>2</sup>
+            {home.area.toLocaleString()} m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
           <Key />
-          <p>$1,200,000</p>
+          <p>${home.price.toLocaleString()}</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
       </div>
+    );
+  });
 
-      <div className="home">
-        <img src={img2} alt="House 2" className="home__img" />
-        <HeartFull />
-        <h5 className="home__name">Modern Glass Villa</h5>
-        <div className="home__location">
-          <Pin />
-          <p>Canada</p>
-        </div>
-        <div className="home__rooms">
-          <Male />
-          <p>6 rooms</p>
-        </div>
-        <div className="home__area">
-          <Expand />
-          <p>
-            450 m<sup>2</sup>
-          </p>
-        </div>
-        <div className="home__price">
-          <Key />
-          <p>$2,750,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-      </div>
-
-      <div className="home">
-        <img src={img3} alt="House 3" className="home__img" />
-        <HeartFull />
-        <h5 className="home__name">Cozy Country House</h5>
-        <div className="home__location">
-          <Pin />
-          <p>UK</p>
-        </div>
-        <div className="home__rooms">
-          <Male />
-          <p>4 rooms</p>
-        </div>
-        <div className="home__area">
-          <Expand />
-          <p>
-            250 m<sup>2</sup>
-          </p>
-        </div>
-        <div className="home__price">
-          <Key />
-          <p>$850,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-      </div>
-
-      <div className="home">
-        <img src={img4} alt="House 4" className="home__img" />
-        <HeartFull />
-        <h5 className="home__name">Large Rustical Villa</h5>
-        <div className="home__location">
-          <Pin />
-          <p>Portugal</p>
-        </div>
-        <div className="home__rooms">
-          <Male />
-          <p>6 rooms</p>
-        </div>
-        <div className="home__area">
-          <Expand />
-          <p>
-            480 m<sup>2</sup>
-          </p>
-        </div>
-        <div className="home__price">
-          <Key />
-          <p>$1,950,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-      </div>
-
-      <div className="home">
-        <img src={img5} alt="House 5" className="home__img" />
-        <HeartFull />
-        <h5 className="home__name">Majestic Palace House</h5>
-        <div className="home__location">
-          <Pin />
-          <p>Germany</p>
-        </div>
-        <div className="home__rooms">
-          <Male />
-          <p>18 rooms</p>
-        </div>
-        <div className="home__area">
-          <Expand />
-          <p>
-            4230 m<sup>2</sup>
-          </p>
-        </div>
-        <div className="home__price">
-          <Key />
-          <p>$9,500,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-      </div>
-
-      <div className="home">
-        <img src={img6} alt="House 6" className="home__img" />
-        <HeartFull />
-        <h5 className="home__name">Modern Familiy Apartment</h5>
-        <div className="home__location">
-          <Pin />
-          <p>Italy</p>
-        </div>
-        <div className="home__rooms">
-          <Male />
-          <p>3 rooms</p>
-        </div>
-        <div className="home__area">
-          <Expand />
-          <p>
-            180 m<sup>2</sup>
-          </p>
-        </div>
-        <div className="home__price">
-          <Key />
-          <p>$600,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-      </div>
-    </section>
-  );
+  return <section className="homes">{home}</section>;
 };
 export default Homes;
